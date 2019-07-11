104d1cb81e08467a4c869d6f6c57b1a0c14f63d2
fatimaaman1234 <49521217+fatimaaman1234@users.noreply.github.com>
2019-07-11 16:59:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let whatsApp = [
+
+    {
+        name: "fatima",
+        number: "0345678127",
+        message: "yoo bro",
+        lastSeen: "3:13 am",
+        aboutContact1:["lazy","clever","funny"],
+    },
+    {
+        name: "sajid",
+        number: "0345678127",
+        message: "yoo bro",
+        lastSeen: "3:15 am",
+        aboutContact2:["selfish","clever","stubborn"],
+    },
+    {
+        name: "farah",
+        number: "0345678127",
+        message: "yoo bro",
+        lastSeen: "3:18 am",
+        aboutContact3:["kind","smily","funny"],
+    },
+    {
+        name: "zehra",
+        number: "0345678127",
+        message: "yoo bro",
+        lastSeen: "3:19 am",
+        aboutContact4:["clever","funny"],
+    }
+
+]
+
+
+
+
+
